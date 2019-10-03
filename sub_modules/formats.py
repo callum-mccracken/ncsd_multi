a8ebc05ea84e01564c1a2e6969cd247bf2619c0b
callum-mccracken <callum189@gmail.com>
2019-10-03 10:37:58 -0700

    a

--- sub_modules/formats.py
+++ sub_modules/formats.py
@@ -50,7 +50,7 @@ cedar_batch_format = """#!/bin/bash
 #SBATCH --nodes={nodes}               # number of 48-cpu nodes
 #SBATCH --tasks-per-node={tasks_per_node}      # mpi tasks per node (max 48)
 #SBATCH --mem={mem}                 # to use full nodes, set this to zero
-#SBATCH --mem-per-cpu={mem_per_core}     # memory per CPU, in GB
+#SBATCH --mem-per-cpu={mem_per_core}G     # memory per CPU
 #SBATCH --time={time}           # time (DD-HH:MM)
 #SBATCH --output={output}
 
